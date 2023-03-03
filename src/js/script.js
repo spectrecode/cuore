f03734014f45c8e94b9c1fbba014d27dae5fb0c2
christy castro <christy@aureolab.cl>
2023-03-03 13:04:37 -0500

    correccion mvp

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,10 @@
-
 $('.owl-carousel').owlCarousel({
     // stagePadding: 50,
     loop:true,
     margin:20,
     responsive:{
         0:{
-            items:1.15
+            items:1
         },
         600:{
             items:3
@@ -15,3 +14,25 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+var img1Mobile = function(){
+    var width        = $(window).width();
+
+    $('.gallery_cards').each(function(){
+        if (width < 768){
+            $(this).addClass('.owl-carousel')
+        } else{
+            $(this).removeClass('.owl-carousel')
+        }
+    })
+
+}
+
+
+$(document).ready(function(){
+    img1Mobile()
+});
+
+$(window).resize(function(){
+    img1Mobile()
+});
